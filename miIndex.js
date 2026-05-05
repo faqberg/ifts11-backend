1638ad487ec2f20415a28d8c839d7b602573ecd7
Facu Berg <faqberg@gmail.com>
2026-05-05 10:30:00 -0300

    Ejercicio 2

--- miIndex.js
+++ miIndex.js
@@ -1,1 +1,7 @@
-console.log("Paquete personalizado inicializado correctamente");
+const colors = require("colors");
+
+console.log("Mensaje en verde".green);
+console.log("Mensaje en azul".blue);
+console.log("Mensaje en rojo".red);
+console.log("Mensaje con arcoíris".rainbow);
+console.log("Texto con fondo amarillo".bgYellow.black);
